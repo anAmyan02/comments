def297073128348e5d461d292f055f4ea70742bf
anAmyan02 <136964764+anAmyan02@users.noreply.github.com>
2023-06-24 16:32:23 +0400

    Add files via upload

--- comments.js
+++ comments.js
@@ -1,31 +1,31 @@
-// let firstDiv = document.querySelector(".firstDiv");
-// let send = document.querySelector("button");
-// let nameInput = document.querySelector(".name");
-// let comInput = document.querySelector(".comment");
+let firstDiv = document.querySelector(".firstDiv");
+let send = document.querySelector("button");
+let nameInput = document.querySelector(".name");
+let comInput = document.querySelector(".comment");
 
-// let commentsArr = JSON.parse(localStorage.getItem("comments")) || [];
+let commentsArr = JSON.parse(localStorage.getItem("comments")) || [];
 
-// toDoListArr.forEach(commentObj => {
-//   let commentText = document.createElement("div");
-//   firstDiv.appendChild(commentText);
-//   commentText.textContent = commentObj.name + ": " + commentObj.comment;
-// });
+commentsArr.forEach(commentObj => {
+  let commentText = document.createElement("div");
+  firstDiv.appendChild(commentText);
+  commentText.textContent = commentObj.name + ": " + commentObj.comment;
+});
 
-// send.addEventListener("click", function () {
-//   let commentText = document.createElement("div");
-//   firstDiv.appendChild(commentText);
-//   commentText.textContent = nameInput.value + ": " + comInput.value;
+send.addEventListener("click", function () {
+  let commentText = document.createElement("div");
+  firstDiv.appendChild(commentText);
+  commentText.textContent = nameInput.value + ": " + comInput.value;
 
-//   let commentObj = {
-//     name: nameInput.value,
-//     comment: comInput.value,
-//   };
+  let commentObj = {
+    name: nameInput.value,
+    comment: comInput.value,
+  };
 
-//   toDoListArr.push(commentObj);
+  commentsArr.push(commentObj);
 
-//   localStorage.setItem("comments", JSON.stringify(toDoListArr));
+  localStorage.setItem("comments", JSON.stringify(commentsArr));
 
-//   nameInput.value = "";
-//   comInput.value = "";
-//   // localStorage.clear()
-// });
+  nameInput.value = "";
+  comInput.value = "";
+//   localStorage.clear()
+});
